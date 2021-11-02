9619a602a3125ab2cfd5b2f1e733292bba262e36
Dario <dario.defrancisci92@gmail.com>
2021-11-02 18:25:44 +0100

    carico tabella

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -2,7 +2,34 @@
 
 @section('content')
     <div class="container">
-        <h1>indice fumetti</h1>
-        @dump($comics);
+        <div class="row">
+            <div class="col-12">
+                <h1>Tutte i miei Fumetti</h1>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Title</th>
+                            <th scope="col">Price</th>
+                            <th scope="col">Series</th>
+                            <th scope="col">Sale_date</th>
+                            <th scope="col">Type</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($comics as $comic)
+                            <tr>
+                                <th scope="row">{{ $comic['id'] }}</th>
+                                <td>{{ $comic['title'] }}</td>
+                                <td>{!! $comic['price'] !!}</td>
+                                <td>{{ $comic['series'] }}</td>
+                                <td>{!! $comic['sale_date'] !!}</td>
+                                <td>{{ $comic['type'] }}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </div>
 @endsection
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,23 +1,20 @@
 <div class="container">
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
-          <a class="navbar-brand" href="#">Navbar</a>
+          <a class="navbar-brand" href="#">I miei Fumetti</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        <a class="nav-link" href="{{route('pagina-home')}}">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Features</a>
+                        <a class="nav-link" href="#">Inserisci un fumetto</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Pricing</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link disabled">Disabled</a>
+                        <a class="nav-link" href="{{route('comics.index')}}">Lista dei Fumetti</a>
                     </li>
                 </ul>
             </div>
