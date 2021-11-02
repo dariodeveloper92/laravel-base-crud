e801cb52df7df1ea295b3220aafdc953229a5d39
Dario <dario.defrancisci92@gmail.com>
2021-11-02 17:16:58 +0100

    upgrade comics

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
     <div class="container">
-        <h1>benvenuti</h1>
+        <h1>indice fumetti</h1>
+        @dump($comics);
     </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,4 +1,18 @@
-{{-- importo i partials --}}
-@include('partials.header')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    {{-- importo css --}}
+    <link rel="stylesheet" href="{{ asset('css/app.css')}}">
+    <title>Comics</title>
+</head>
+<body>
+    {{-- importo i partials --}}
+    @include('partials.header')
+    @yield('content')
 
-@include('partials.footer')
+    @include('partials.footer')
+</body>
+</html>
